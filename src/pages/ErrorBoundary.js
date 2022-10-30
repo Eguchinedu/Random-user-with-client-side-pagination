073be91d8d8be41a6d6a24d656bc0cae6ccb368b
Egu Chinedu <eguchinedu18@gmail.com>
2022-10-30 12:52:20 +0100

    Edited the home.css and errorboundary.js file

--- src/pages/ErrorBoundary.js
+++ src/pages/ErrorBoundary.js
@@ -1,38 +1,38 @@
-import React from 'react'
-import {Component} from 'react'
-import { Link } from 'react-router-dom'
-
+import React from "react";
+import { Component } from "react";
+import { Link } from "react-router-dom";
 
 export class ErrorBoundary extends Component {
-    constructor(props) {
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
-        hasError: false,
-        error: null,}
-    }
+      hasError: false,
+      error: null
+    };
+  }
 
-    static getDerivedStateFromError(error) {
-        // Update state so the next render will show the fallback UI.
-        return {hasError: true, error: error}
+  static getDerivedStateFromError(error) {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true, error: error };
+  }
+  componentDidCatch(error, errorInfo) {
+    // Catch errors in any components above and re-render with error message
+    console.log("logging", error, errorInfo);
+  }
+  render() {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return (
+        <>
+          <h1 className="error">Oops!!something went wrong!!</h1>
+          <div className="button">
+            <Link to="/" className="btn">
+              Back to Home
+            </Link>
+          </div>
+        </>
+      );
     }
-    componentDidCatch(error, errorInfo) {
-        // Catch errors in any components above and re-render with error message
-    console.log('logging', error, errorInfo)
-    }
-    render() {
-        if (this.state.hasError) {
-            // You can render any custom fallback UI
-            return (
-              <>
-                <h1 className="error">something went wrong!!</h1>
-                <div className="button">
-                  <Link to="/" className="btn">
-                    Back to Home
-                  </Link>
-                </div>
-              </>
-            );
-        }
-        return this.props.children
-    }
+    return this.props.children;
+  }
 }
